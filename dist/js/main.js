e67f7952314448f4d92b15ce45936d1d50177512
SidVZ <sidvz@protonmail.com>
2022-01-16 23:36:29 +0530

    slider menu animation complete

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,15 +1,21 @@
 const menuBtn = document.querySelector('.menu-btn');
 const burger = document.querySelector('.menu-btn__burger');
+const nav = document.querySelector('nav');
+const menuNav = document.querySelector('.menu-nav');
 
 let showMenu = false;
 
 function toggleMenu() {
     if(!showMenu) {
         burger.classList.add('open');
+        nav.classList.add('open')
+        menuNav.classList.add('open')
         
         showMenu = true;
     } else {
         burger.classList.remove('open');
+        nav.classList.remove('open');
+        menuNav.classList.remove('open');
 
         showMenu = false;
     }
